602de8e80d99307ef8d08f474eadfbcae0602a1e
meghana <meghana@192.168.43.112>
2021-07-31 06:30:01 -0700

    dont check this

---
+++ rootservice/src/main/java/com/example/demo/RootserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootserviceApplication.class, args);
+	}
+
+}
